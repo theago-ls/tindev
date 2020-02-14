d41003fb37e3b0b6854a76bb973e63ed694cebdc
Thiago L <thiagosup1@gmail.com>
2020-02-14 00:42:25 -0400

    First commit

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const routes = require('./routes.js');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers [user] = socket.id;
+});
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-9vl1t.mongodb.net/omnistack8?retryWrites=true&w=majority', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
---
+++ tindev/pages/Login.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import { KeyboardAvoidingView, Platform, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import logo from '../src/assets/logo.png';
+
+import api from '../src/services/api';
+
+export default function Login({ navigation }){
+    const [user, setUser] = useState('');
+    
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user){
+                navigation.navigate('Main', { user });
+            }
+        })
+    }, []);
+
+    async function handleLogin(){
+        const response = await api.post('/devs', { username: user });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', { user: _id });
+    }
+
+
+    return( 
+        <KeyboardAvoidingView 
+            style={styles.container}
+            behavior="padding"
+            enabled={Platform.OS === 'ios'}
+        >
+            <Image source={logo} />
+
+            <TextInput 
+                autoCorrect={false}
+                autoCapitalize="none"
+                placeholder="Digite seu usuário no Github"
+                style={styles.input}
+                value={user}
+                onChangeText={setUser}
+            />
+
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                <Text style={styles.buttonText}>Enviar</Text>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
+    }, 
+
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15,
+    },
+
+    button: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#DF4723',
+        borderRadius: 4,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
